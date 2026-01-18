bc87440e81720b088bc4b34ab032d144f46d236f
musstafaKocc <musstafa.kocc@gmail.com>
2026-01-18 11:50:42 +0300

    Add basic variable example

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+var firstName = "Mustafa";
+var lastName = "Koç";
+var age = 29;
+
+Console.WriteLine($"Hello, my name is {firstName} {lastName}, and I ama {age} years old.");

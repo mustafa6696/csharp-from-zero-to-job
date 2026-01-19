1d8332b02035629bf6e57bf37da441f6f2223248
musstafaKocc <musstafa.kocc@gmail.com>
2026-01-19 09:30:56 +0300

    Add menu handling using switch statement

---
+++ 04-Switch/SwitchApp/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Choose an option: ");
+Console.WriteLine("1 - Add");
+Console.WriteLine("2 - Update");
+Console.WriteLine("3 - Delete");
+
+Console.Write("Your choice: ");
+var input = Console.ReadLine();
+
+switch (input)
+{
+    case "1":
+        Console.WriteLine("Add operation selected.");
+        break;
+
+    case "2":
+        Console.WriteLine("Update operation selected.");
+        break;
+
+    case "3":
+        Console.WriteLine("Delete operation selected.");
+        break;
+
+    default:
+        Console.WriteLine("Invalid selection");
+        break;
+}
+
+//var message = input switch
+//{
+//    "1" => "Add operation selected.",
+//    "2" => "Update operation selected.",
+//    "3" => "Delete operation selected.",
+//    _ => "Invalid selection"
+//};

d736c6a25706b4e41fdd22d1a8af0d0a1acc7c78
musstafaKocc <musstafa.kocc@gmail.com>
2026-01-19 08:46:35 +0300

    Add age validation using if else conditions

---
+++ 03-IfElse/03-IfElse/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your age: ");
+var input = Console.ReadLine();
+
+if (int.TryParse(input, out var age))          
+{
+	if (age < 18)
+	{
+		Console.WriteLine("Access denied. You must be at least 18 years old.");
+	}
+    else
+    {
+        Console.WriteLine("Access granted.");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input!");
+}

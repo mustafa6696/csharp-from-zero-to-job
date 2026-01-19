f8dd46db1540592d4e85d8ce223ff2837b426782
musstafaKocc <musstafa.kocc@gmail.com>
2026-01-19 08:36:13 +0300

    Add user input handling with safe type conversion

---
+++ 02-UserInputApp/02-UserInputApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your age: ");
+var input = Console.ReadLine();
+
+if (int.TryParse(input, out var age))
+{
+    Console.WriteLine($"Your age after 5 years: {age + 5}");
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
